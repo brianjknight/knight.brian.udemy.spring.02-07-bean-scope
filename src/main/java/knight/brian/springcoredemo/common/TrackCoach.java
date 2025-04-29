7ddbd036097ac43304017b59c5a992e81be5e7bd
Brian Knight <knight.j.brian@gmail.com>
2025-04-29 09:53:25 -0600

    cloned repo

---
+++ src/main/java/knight/brian/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,18 @@
+package knight.brian.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a 5K.";
+    }
+}
---
+++ src/main/java/knight/brian/springcoredemo/rest/DemoController.java
@@ -0,0 +1,31 @@
+package knight.brian.springcoredemo.rest;
+
+import knight.brian.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    // Example of constructor injection
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    // Example of setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
