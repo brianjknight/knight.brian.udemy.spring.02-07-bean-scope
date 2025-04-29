44643f85255a2cfa83b0621bc1c00b3d672ea18e
Brian Knight <knight.j.brian@gmail.com>
2025-04-29 10:02:34 -0600

    completed #53

--- src/main/java/knight/brian/springcoredemo/common/CricketCoach.java
+++ src/main/java/knight/brian/springcoredemo/common/CricketCoach.java
@@ -1,10 +1,11 @@
 package knight.brian.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach() {
--- src/main/java/knight/brian/springcoredemo/rest/DemoController.java
+++ src/main/java/knight/brian/springcoredemo/rest/DemoController.java
@@ -10,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // Example of constructor injection
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach,
+                          @Qualifier("cricketCoach") Coach theAnotherCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
+        anotherCoach = theAnotherCoach;
     }
 
     // Example of setter injection
@@ -27,5 +30,10 @@ public class DemoController {
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach " + (myCoach == anotherCoach);
     }
 }
